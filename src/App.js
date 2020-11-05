0e94c1b9aa51198cb6a40db365823f3a12e01ceb
gallo <g2gamingempresa@gmail.com>
2020-11-04 21:35:36 -0300

    version3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there!
+          Soy mooooy crack!
         </p>
         <a
           className="App-link"
